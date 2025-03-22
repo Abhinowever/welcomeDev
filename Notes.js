346a7c7dc4ecf5adbbf90c03a2a9dfaa33aaa19c
Abhinav Singh <abhinowever@gmail.com>
2025-03-22 11:38:35 +0530

    updating notes

--- Notes.js
+++ Notes.js
@@ -1,10 +1,14 @@
 /* the types of message displaying is console.log, console.error, console.warn, console.confirm, console.prompt, console.info, console.debug, console.exception, console.clear */
-console.log('This is a log message');
-console.error('This is an error message');
-console.warn('This is a warning message');
-console.confirm('This is a confirmation message');
-console.prompt('This is a prompt message');
-console.info('This is an info message');
-console.debug('This is a debug message');
-console.exception('This is an exception message');
-console.clear();
+
+
+// console.clear();
+// console.log('This is a log message');
+// console.error('This is an error message');
+// console.warn('This is a warning message');
+// console.debug('This is a debug message');
+// console.info('This is an info message');
+// console.exception('This is an exception message');
+// console.prompt('This is a prompt message');
+// console.confirm('This is a confirmation message');
+
+
