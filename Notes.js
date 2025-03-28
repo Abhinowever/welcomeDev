cbd6cbb4d1944906101b066317b39e6f227bae78
Abhinav Singh <abhinowever@gmail.com>
2025-03-28 18:08:52 +0530

    Adding hover

--- Notes.js
+++ Notes.js
@@ -1,6 +1,7 @@
 /* the types of message displaying is console.log, console.error, console.warn, console.confirm, console.prompt, console.info, console.debug, console.exception, console.clear */
 
 
+
 // console.clear();
 // console.log('This is a log message');
 // console.error('This is an error message');
@@ -11,4 +12,21 @@
 // console.prompt('This is a prompt message');
 // console.confirm('This is a confirmation message');
 
+
+/* DOM Manipulation
+
+  4 Pillars of DOM Manipulation are:
+
+  1. Selecting Elements - getElementById, getElementsByClassName, getElementsByTagName, querySelector, querySelectorAll(selects all elements that match a specified CSS selector)
+  2. Manipulating Elements - innerHTML, textContent, value, attributes, className, classList, style, addEventListener, removeEventListener
+  3. Creating Elements - createElement, createTextNode, appendChild, insertBefore, removeChild, replace
+  4. Deleting Elements - removeChild, remove, replace
+*/
+
+
+
+
+
+
+// JS Logics
 
